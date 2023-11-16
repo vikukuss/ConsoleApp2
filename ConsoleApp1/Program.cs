ca7657f8182d85c8f2c5fcccf2fd44a6d8f0c9f5
vikukuss <kosinovDE@C7A109RU-12NN>
2023-11-16 11:07:37 +0300

    изменения 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,5 +5,21 @@ class Program
     {
         Console.WriteLine("хаха");
         Console.WriteLine("хихи");
+
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+        Console.WriteLine("хохо");
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+        Console.WriteLine("хохо");
+        Console.WriteLine("хуху");
+        Console.WriteLine("хуху");
     }
 }
